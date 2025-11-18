866c9032d386eae3f666642d534513cd7bd20394
lloydagola <lloydagola@gmail.com>
2025-11-18 21:33:19 +0300

    fixed options

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,10 +4,21 @@
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    println!("{}", hour_of_day);
+    if hour_of_day > 23{
+        None
+    }
+    else if hour_of_day >= 22{
+        Some(0)
+    }
+    else{
+        Some(5)
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    maybe_ice_cream(7);
 }
 
 #[cfg(test)]
@@ -18,7 +29,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let ice_creams = maybe_ice_cream(12);
+        let ice_creams = maybe_ice_cream(12).unwrap();
 
         assert_eq!(ice_creams, 5); // Don't change this line.
     }
